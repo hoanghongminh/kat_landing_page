cdceea4367a3f04a21c4d49d12c2882b3f961558
Minhhoang <hoangminh881997@gmail.com>
2024-07-02 17:48:08 +0700

    Create Landing v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,53 +34,66 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="">
-      <LandingLayout>
-        <section className="banner w-full grid grid-cols-12 gap-8 items-center justify-between flex-col-reverse py-12 md:flex-row max-md:justify-items-center">
-          <div className="relative z-20 banner-content grid col-span-12 max-md:order-2 md:col-span-8 text w-full max-w-[630px] max-md:mt-12 max-md:text-center max-md:flex max-md:flex-col max-md:items-center max-md:pt-10">
-            <img
-              className="w-[270px] img-logo mb-4"
-              src="/images/CatCafeLogo.png"
-              alt=""
-            />
-            <h1 className="mb-2 lg:mb-4">
-              <p className="text-2xl md:text-3xl lg:text-4xl">
-                Let’s spread the coziness across the world
-              </p>
-            </h1>
-            <div className="mb-6 text-base lg:text-xl">
-              Manage and grow your dream coffee shop like never before
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300;400;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <main className="">
+        <LandingLayout>
+          <section className="banner w-full flex">
+            <div className="content-banner border-r border-black flex flex-col items-center justify-between text-center relative overflow-hidden">
+              <div className="flex flex-col gap-12 items-center h-full overflow-hidden relative">
+                <div className="my-12 px-12 flex flex-col items-center justify-center max-md:mb-24">
+                  <div className="flex items-center mb-3">
+                    <img
+                      className="w-[187px] md:w-[225px]"
+                      src="/images/logo.png"
+                      alt=""
+                    />
+                  </div>
+                  <h1 className="mb-3">Earn token with Kat Kafe</h1>
+                  <h4 className="mb-4">
+                    Manage staffs, complete quests, invite friends and become
+                    the best owner
+                  </h4>
+                  <div className="flex gap-2 max-xl:flex-wrap max-xl:justify-center">
+                    <a
+                      href="https://capylabs.gitbook.io/katkafe"
+                      target="_blank"
+                    >
+                      <button>Gitbook</button>
+                    </a>
+                    <a
+                      href="https://www.beautiful.ai/player/-O-LUT5aQfeKiOOMQcID"
+                      target="_blank"
+                    >
+                      <button>Whitepaper</button>
+                    </a>
+                    <a href="https://t.me/katkafe_bot/KatKafe" target="_blank">
+                      <button>Telegram</button>
+                    </a>
+                  </div>
+                </div>
+                <img
+                  className="catOne w-[128px] h-[117px] md:w-[210px] md:h-[192px] absolute -left-20 -bottom-10 md:-bottom-20"
+                  src="/images/catOne.png"
+                  alt=""
+                />
+                <img
+                  className="catTwo w-[128px] h-[136px] md:w-[210px] md:h-[222px] -rotate-12 absolute -right-12 -bottom-10 md:-bottom-20"
+                  src="/images/catTwo.png"
+                  alt=""
+                />
+              </div>
             </div>
-            <div className="flex justify-center items-center gap-1 browser bg-white shadow-md w-[164px] h-12">
-              <img className="h-6 w-6" src="/images/telegram.png" alt="" />
-              <p className="text-base ml-1">Telegram</p>
-            </div>
-          </div>
-          <div className="image w-fit col-span-12 md:col-span-4 lg:col-span-4 max-md:order-1 relative max-md:pt-12 max-md:flex max-md:justify-center">
-            <img
-              className="banner-home mx-auto"
-              src="/images/Home.png"
-              alt=""
-            />
-            <img
-              className="absolute img-join-us z-50"
-              src="/images/join-us.png"
-              alt=""
-            />
-            <img
-              className="absolute img-bundle z-50"
-              src="/images/bundle.png"
-              alt=""
-            />
-            <img
-              className="absolute img-gem z-50"
-              src="/images/gem.png"
-              alt=""
-            />
-          </div>
-        </section>
-        {/* <img className="w-12 h-12" src="/images/coin.png" alt="" /> */}
-      </LandingLayout>
-    </main>
+          </section>
+        </LandingLayout>
+      </main>
+    </>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,35 +3,21 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className="bg-footer">
-      <footer className="flex justify-between px-5 py-2 md:py-4 container mx-auto max-md:flex-col max-md:items-center max-md:px-4 max-md:py-6">
-        <div>
-          <a href="./">
-            <img
-              className="w-12 logo object-cover"
-              src="/images/CatCafeLogo.png"
-              alt=""
-            />
-          </a>
-        </div>
-        <nav className="flex items-center max-md:flex-col max-md:mt-6">
-          <ul className="flex gap-8">
+    <div className="bg-footer border-t border-black w-full h-[64px] flex">
+      <footer className="flex justify-between px-5 xl:container mx-auto max-md:items-center max-md:px-4">
+        <nav className="w-full flex items-center">
+          <ul className="flex gap-8 justify-between w-full">
             <a href="" target="_blank" rel="noopener noreferrer">
               <li className="cursor-pointe hover:text-[#498ee4]">
                 Privacy Policy
               </li>
             </a>
             <a href="" target="_blank" rel="noopener noreferrer">
-              <li className="cursor-pointer hidden md:block hover:text-[#498ee4]">
+              <li className="cursor-pointer hover:text-[#498ee4]">
                 Term and Conditions
               </li>
             </a>
           </ul>
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <p className="cursor-pointer md:hidden mt-4 hover:text-[#498ee4]">
-              Term and Conditions
-            </p>
-          </a>
         </nav>
       </footer>
     </div>
